58c71de4d34826baed5d371593c1687f474753fc
Abu <yqelz112@gmail.com>
2026-02-09 11:14:16 +0300

    Add Veggie Supreme pizza object to Script.js

---
+++ Script.js
@@ -0,0 +1,10 @@
+{
+    id: 10,  // الرقم التالي في التسلسل
+    name: "Veggie Supreme",
+    category: "vegetarian",  // "classic", "specialty", أو "vegetarian"
+    price: 21.99,
+    description: "Loaded with fresh vegetables and three types of cheese.",
+    ingredients: "Tomato sauce, mozzarella, bell peppers, mushrooms, onions, olives, spinach",
+    rating: 4,  // من 1 إلى 5 نجوم
+    image: "https://images.unsplash.com/photo-1595708684082-a173bb3a06c5?ixlib=rb-4.0.3&auto=format&fit=crop&w=1374&q=80"
+}
